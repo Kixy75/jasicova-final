ca5efcfef27a00faa8285da908507a04e3635c85
Kixy75 <kika.jasicova@gmail.com>
2018-04-28 21:00:06 +0200

    prvy commit aj s dizajnom

---
+++ my-app/src/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import { Router, browserHistory, Route, Link } from 'react-router';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+    render() {
+        return (
+            <div className="App">
+            <header className="App-header">
+            <h1 className="App-title"></h1>
+        </header>
+        <p className="App-intro">
+           <code>src/App.js</code> and save to reload.
+        </p>
+        </div>
+    );
+    }
+}
+
+export default App;
